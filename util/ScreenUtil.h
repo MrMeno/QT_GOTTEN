f614a73c00769da2417481197307b93af772e299
MrMeno <myzonemin@qq.com>
2020-05-25 17:42:02 +0800

    add new windows

--- util/ScreenUtil.h
+++ util/ScreenUtil.h
@@ -12,36 +12,15 @@
 #include <QTimer>
 #include "util/ScreenCaputredEvent.h"
 #include <QPoint>
-struct HandleRect
-{
-    HWND hwnd;
-    RECT wndRect;
-    HandleRect() {}
-};
-
 class ScreenUtil:public QObject
 {
     Q_OBJECT
 public:
-    ScreenCaputredEvent *ScreenCaputred;
-    QEvent *ScreenCaputreCancelled;
-    HandleRect GetCursorPosWndRect(QPoint p);
-    bool IsPointInRect(QPoint p,RECT t);
-    void StartCaputre(int timeOutSeconds,QObject sender);
-    void StartCaputre(int timeOutSeconds, QSize size);
-    void OnScreenCaputred(QObject sender,RECT clipRegion);
-    void OnScreenCaputreCancelled(QObject sender);
-    void OnWaitTimmerTick(QObject sender,QEvent e);
     explicit ScreenUtil(QObject *parent = nullptr);
 signals:
     void sendData(int,QSize);
 private:
-    RECT *waitRect;
-    QTimer waitTimmer;
-    QPoint *waitPoint;
-    QObject *paramSender;
-    QList<HandleRect> *hwndRectList;
-    void InitWndHandleRect();
+
 };
 
 #endif // SCREENUTIL_H
